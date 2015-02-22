dc2b522d7ae6a2a53efb5c917a5389c27a39be7f
Amit Gupta <agupta@pmc.com>
2015-02-22 15:15:11 +0530

    version bumped to v1.0

--- ig-custom-metaboxes.php
+++ ig-custom-metaboxes.php
@@ -2,14 +2,14 @@
 /*
 Plugin Name: iG:Custom Metaboxes
 Description: A custom field manager which provides an object oriented & clean API to create custom metaboxes and attach custom input fields to them which are saved as post meta automatically on post save.
-Version: 1.0 RC1
+Version: 1.0
 Author: Amit Gupta
 Author URI: http://igeek.info/
 License: GPLv2 or later
 */
 
 define( 'IG_CUSTOM_METABOXES_ROOT', __DIR__ );
-define( 'IG_CUSTOM_METABOXES_VERSION', '1.0-rc1' );
+define( 'IG_CUSTOM_METABOXES_VERSION', '1.0' );
 
 function ig_custom_metaboxes_loader() {
 	/*
